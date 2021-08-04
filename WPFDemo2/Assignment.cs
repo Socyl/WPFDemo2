81d4a664c9a48a3f4a630b628cf74ca969b77fb6
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-08-04 17:03:05 -0600

    Add project files.

---
+++ WPFDemo2/Assignment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemo2
+{
+    class Assignment
+    {
+        public string Title { get; set; }
+
+        private int score;
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                if(value>=0 && value <= 100)
+                {
+                    score = value;
+                }
+                else { throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return Title+ " " +Score;
+        }
+    }
+}
---
+++ WPFDemo2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WPFDemo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Assignment> assignments;
+        public MainWindow()
+        {
+            InitializeComponent();
+            assignments = new List<Assignment>();
+            lbScores.ItemsSource = assignments;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student();
+            student.FirstName = txbFirstName.Text;
+            student.LastName = txbLastName.Text;
+            student.StudentNumber = txbStudentNumber.Text;
+            student.Major = txbMajor.Text;
+
+
+            //listbox section in submit
+            List<Assignment> assignmentsCopy = new List<Assignment>(assignments);
+  
+            /*
+            foreach(Assignment assignment in lbScores.Items)
+            {
+                assignments.Add(assignment);
+
+            }
+            */
+
+
+            student.Scores = assignmentsCopy;
+
+            txbResults.Text = student.ToString();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Assignment assignment = new Assignment();
+            assignment.Title = txbTitle.Text;
+            int scoreInt = int.Parse(txbScore.Text);
+            assignment.Score = scoreInt;
+            assignments.Add(assignment);
+            lbScores.Items.Refresh();
+        }
+    }
+}
